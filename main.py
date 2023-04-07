2bff830f9c31ffcd10186c51b44217bb06a262e2
Phrit Nakgate <boss5500216@gmail.com>
2023-04-07 22:59:06 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+# --- Enroll Test --- #
+myenroll = {"username": "ffwatcharin", "refcode": "HARD001"}
+print(myenroll)
+x = requests.post("http://localhost:8000/addcart", data=json.dumps(myenroll))
+print(x)
+y = requests.get("http://localhost:8000/cart")
+print(y)
