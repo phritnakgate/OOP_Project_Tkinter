224ca2003c5c5fa3168cbd972501763a3aed6511
Yongsuk <84030256+MAOK-Yongsuk@users.noreply.github.com>
2023-04-08 00:40:41 +0700

    fix (backend) : change name

--- api.py
+++ api.py
@@ -3,6 +3,8 @@ from user import *
 from system import *
 from datetime import datetime
 from courses import *
+from exam import *
+from dto import *
 
 # --- Creation Test --- #
 teacher = Teacher("teach1", "123456", "teacher1@gmail.com", "tea", "cher", "Male", datetime(2003, 12, 4), "D.Eng",
@@ -12,10 +14,11 @@ admin = Admin("admin", "admin1234", "admin1@gmail.com", "ad", "min", "Male", dat
 student = Student("ffwatcharin", "firstbigdick", "ffwatcharin@gmail.com", "Watcharin", "Humthong", "Male",
                   datetime(2004, 1, 16), "Undergraduated", "Nonthaburi", "Thailand")
 
-course = Courses("SOFT001", "Object Oriented Programming", "Learn writing oop", "teach1", "Software", "All Ages",
+course1 = Courses("SOFT001", "Object Oriented Programming", "Learn writing oop", "teach1", "Software", "All Ages",
                  "To understanding OOP", "10", "10", datetime.now(), "teacher1@gmail.com")
 course2 = Courses("HARD001", "Basic Arduino", "Learn Basic Arduino", "teach1", "Hardware", "All Ages",
                   "To understanding Arduino", "10", "10", datetime.now(), "teacher1@gmail.com")
+
 # --- Build Test --- #
 course_system = CourseSystem()
 
@@ -29,6 +32,9 @@ print(course_system.get_user_db())
 
 student.set_enrolled_course('enroll', course)  # Student has enrolled SOFT001.
 
+oop_exam = CourseExam(course1.get_title)  
+
+#------------------------------- API -----------------------------------#
 app = FastAPI()
 
 @app.get("/", tags=['root'])
@@ -47,12 +53,28 @@ async def login():
 async def courses():
     pass
 
+# ------------------------------- Exam API --------------------------------#
+@app.post("/exam/question and answer")
+async def add_question(data : Problems):
+    q_list = []
+    for q , a in data.questions:
+        q_list.append(ExamItem(q, a))
+    oop_exam.add_question_ans(q_list)
+    return {"message": "Question and Answer added successfully"}
 
-# --- Enroll API --- #
+@app.get("/exam")
+async def get_exam():
+    return oop_exam._exam_list
+
+@app.post("/exam")
+async def take_exam(student_answers: list):
+    score = oop_exam.do_exam(student_answers)
+    return {"score": score}
+
+# -------------------------------- Enroll API ----------------------------- #
 @app.get("/cart", tags=["Enrollment"])
 async def cart():
     return {"Cart": course_system.get_cart()}
-
 
 @app.post("/addcart", tags=["Enrollment"])
 async def add_cart(cart_item: dict) -> dict:
