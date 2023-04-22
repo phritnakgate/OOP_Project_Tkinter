7c2a05870cc524bf06df66073d60b442f51c269c
Phrit Nakgate <boss5500216@gmail.com>
2023-04-22 22:48:18 +0700

    chore(frontend): change Tk GUI to CTk (except coursecatalog, register)

--- gui/cartgui.py
+++ gui/cartgui.py
@@ -5,7 +5,7 @@ import customtkinter
 import requests
 import json
 
-customtkinter.set_appearance_mode("System")
+customtkinter.set_appearance_mode("Dark")
 customtkinter.set_default_color_theme("blue")
 
 class CartGUI:
